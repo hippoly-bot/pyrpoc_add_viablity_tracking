aa801249117cd1c4ae7106f05673bd74cb192f83
Ishaan <sing1125@purdue.edu>
2025-04-04 15:33:21 -0400

    update

--- pysrs/mains/pyqt_rpoc.py
+++ pysrs/mains/pyqt_rpoc.py
@@ -11,7 +11,7 @@ from PyQt5.QtWidgets import (
     QApplication, QMainWindow, QPushButton, QFileDialog,
     QGraphicsView, QGraphicsScene, QGraphicsPixmapItem,
     QVBoxLayout, QWidget, QTableWidget, QTableWidgetItem,
-    QHBoxLayout, QCheckBox, QLabel
+    QHBoxLayout, QCheckBox, QLabel, QGraphicsEllipseItem
 )
 from PyQt5.QtGui import QPixmap, QPainterPath, QPen, QBrush, QPainter
 from PyQt5.QtCore import Qt, QPointF, QRectF, QPoint
@@ -25,6 +25,7 @@ class ImageViewer(QGraphicsView):
 
         self.drawing = False
         self.current_path = None
+        self.temp_path_item = None
         self.current_points = []
 
         self.show_rois = True # from M click
@@ -35,63 +36,99 @@ class ImageViewer(QGraphicsView):
         self.path_pen = QPen(Qt.red, 2, Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin)
         self.roi_opacity = 0.4
 
+        self.brush_radius = 3
+        self.cursor_brush = QGraphicsEllipseItem(0, 0, self.brush_radius * 2, self.brush_radius * 2)
+        self.cursor_brush.setBrush(QBrush(Qt.blue))
+        self.cursor_brush.setPen(QPen(Qt.NoPen))
+        self.cursor_brush.setZValue(1000)  # Always on top
+        self.cursor_brush.setVisible(False)
+        self.scene().addItem(self.cursor_brush)
+
         self.setMouseTracking(True)
+        self.add_cursor_brush()
 
     def mousePressEvent(self, event):
         # the goal here is for a right click to start the drawing
         # then as the cursor is moved, the drawing is done
         # then when right click is pressed again, the drawing is done
         # i want it to be just like cellpose, but maybe better polygon-ing logic?
-        if event.button() == Qt.RightButton: 
-            if not self.drawing:
-                self.drawing = True
-                self.current_points = []
-                self.current_path = QPainterPath()
+        if event.button() == Qt.RightButton:
+            self.drawing = True
+            self.cursor_brush.setVisible(True)
+            self.current_points = []
+            self.current_path = QPainterPath()
 
-                start_pos = self.mapToScene(event.pos())
-                self.current_path.moveTo(start_pos)
-                self.current_points.append(start_pos)
+            start_pos = self.mapToScene(event.pos())
+            self.current_path.moveTo(start_pos)
+            self.current_points.append(start_pos)
 
-            else:
-                self.drawing = False
-                end_pos = self.mapToScene(event.pos())
-                self.current_points.append(end_pos)
-                self.current_path.lineTo(end_pos)
-                self.current_path.closeSubpath()
-
-                self.roi_count += 1
-                color = self.get_random_color()
-                roi_item = self.scene().addPath(self.current_path)
-                roi_item.setPen(QPen(color, 2))
-                roi_item.setBrush(QBrush(color))
-                roi_item.setOpacity(self.roi_opacity if self.show_rois else 0.0)
-
-                self.roi_items.append(roi_item)
-                self.add_roi_to_table(self.roi_count, self.current_points)
-                self.current_path = None
-                self.current_points = []
-
-        # left clicking should allow for drawing of straight lines
-        # TODO: add line width logic for left clicking
-        # also separate left clicking from right-click anchoring
-        elif self.drawing and event.button() == Qt.LeftButton:
-            pos = self.mapToScene(event.pos())
-            self.current_points.append(pos)
-            self.current_path.lineTo(pos)
-            # maybe just use a QGraphicsPathItem and update setPath
-            self.scene().update()
+            if self.temp_path_item:
+                self.scene().removeItem(self.temp_path_item)
+            self.temp_path_item = self.scene().addPath(self.current_path, self.path_pen)
+            self.temp_path_item.setZValue(999)
 
         super().mousePressEvent(event)
 
     def mouseMoveEvent(self, event):
+        scene_pos = self.mapToScene(event.pos())
+
+        self.cursor_brush.setPos(scene_pos.x() - self.brush_radius, scene_pos.y() - self.brush_radius)
+
         if self.drawing and self.current_path is not None:
-            pos = self.mapToScene(event.pos())
-
-            temp_path = QPainterPath(self.current_path)
-            temp_path.lineTo(pos)
-
-            self.scene().update()
+            if not self.point_in_existing_rois(scene_pos):
+                self.current_path.lineTo(scene_pos)
+                self.current_points.append(scene_pos)
+                if self.temp_path_item:
+                    self.temp_path_item.setPath(self.current_path)
         super().mouseMoveEvent(event)
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.RightButton and self.drawing:
+            self.drawing = False
+            self.cursor_brush.setVisible(False)
+
+            if len(self.current_points) > 2:
+                self.current_path.closeSubpath()
+
+            if self.temp_path_item:
+                self.scene().removeItem(self.temp_path_item)
+                self.temp_path_item = None
+
+            self.roi_count += 1
+            color = self.get_random_color()
+            roi_item = self.scene().addPath(self.current_path)
+            roi_item.setPen(QPen(color, 2))
+            roi_item.setBrush(QBrush(color))
+            roi_item.setOpacity(self.roi_opacity if self.show_rois else 0.0)
+
+            self.roi_items.append(roi_item)
+            self.add_roi_to_table(self.roi_count, self.current_points)
+
+            self.current_path = None
+            self.current_points = []
+
+        super().mouseReleaseEvent(event)
+
+    def add_cursor_brush(self):
+        # DEFENSE DEFENSE DEFENSE bum bum bum
+        if hasattr(self, 'cursor_brush') and self.cursor_brush in self.scene().items():
+            self.scene().removeItem(self.cursor_brush)
+
+        from PyQt5.QtGui import QColor
+        self.brush_radius = 4
+        self.cursor_brush = self.scene().addEllipse(
+            -self.brush_radius, -self.brush_radius,
+            2 * self.brush_radius, 2 * self.brush_radius,
+            QPen(Qt.NoPen),
+            QBrush(QColor(255, 0, 0, 100))
+        )
+        self.cursor_brush.setZValue(10)  # Always on top
+
+    def point_in_existing_rois(self, point):
+        for roi_item in self.roi_items:
+            if roi_item.path().contains(point):
+                return True
+        return False
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_M:
@@ -158,8 +195,10 @@ class MainWindow(QMainWindow):
             pixmap = QPixmap(file_path)
             self.image_scene.clear()
             self.image_scene.addPixmap(pixmap)
-            self.image_scene.setSceneRect(QRectF(pixmap.rect())) # apparently qrect is not good enough
+            self.image_scene.setSceneRect(QRectF(pixmap.rect()))
 
+            # avoid crash on cursor deletion
+            self.image_view.add_cursor_brush()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
